0f3fce6408e0cc4083f9c47db1d9ddac1d139642
jode401 <jeoper21@gmail.com>
2026-03-20 14:55:33 +0000

    hry

--- app.js
+++ app.js
@@ -27,4 +27,8 @@ res.render('red')
   app.get('/',(req,res)=>{
 res.render('index')
   })
+app.use((req, res, next) => {
+  res.status(404).send('ERROR:404');
+});
+
 app.listen(4000, () => console.log(`Server on http://localhost:${4000}`));
